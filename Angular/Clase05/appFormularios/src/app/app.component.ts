c3a2a0757aaa97bb6e289341e060898c9fe50de2
Eduardo Hoyos <edu.hoyos92@gmail.com>
2019-08-08 21:42:38 -0500

    Forms Module

--- Angular/Clase05/appFormularios/src/app/app.component.ts
+++ Angular/Clase05/appFormularios/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,9 +8,40 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
 
-  edadMinima = 18
+  @ViewChild("formulario", { static: true }) f: NgForm
 
-  registrar(f: NgForm) {
-    console.log(f)
+  /*   registrar(f: NgForm) {
+      if (f.valid) {
+        console.log("Valido")
+      } else {
+        console.log("Invalido")
+      }
+    } */
+
+  registrar() {
+    if (this.f.valid) {
+      console.log(this.f.value)
+      console.log(this.f.form.getRawValue())
+      console.log("Valido")
+      this.f.form.reset()
+    } else {
+      console.log("Invalido")
+    }
+  }
+
+  cargarData() {
+    this.f.setValue({
+      nombre: "Sergio",
+      correo: "sergio.hidalgo@prueba.com",
+      contrasena: "123",
+      edad: 39
+    })
+  }
+
+  cargarDataParcial() {
+    this.f.form.patchValue({
+      correo: "sergio.hidalgo@prueba.com",
+      contrasena: "123"
+    })
   }
 }
