54de374208b418f35bc852c7fca833c6b87502b7
Eduardo Hoyos <edu.hoyos92@gmail.com>
2019-08-13 20:46:45 -0500

    ReactiveFormsModule

--- Angular/Clase06/appFormularios/src/app/app.component.ts
+++ Angular/Clase06/appFormularios/src/app/app.component.ts
@@ -10,15 +10,33 @@ export class AppComponent {
 
   group: FormGroup
 
+  listaCorreosGratuitos = ["gmail.com", "yahoo.com", "hotmail.com", "outlook.com"]
+
   constructor() { }
 
   ngOnInit() {
     this.group = new FormGroup({
-      correo: new FormControl(null, [Validators.required, Validators.email]),
+      correo: new FormControl(null, [Validators.required, Validators.email, this.validadorCorreoEmpresarial.bind(this)]),
       nombre: new FormControl("Sergio", Validators.required),
-      contrasena: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      contrasena: new FormControl(null, [Validators.required, Validators.pattern(/^[a-zA-Z]{6,15}$/i)]),
       edad: new FormControl(null, Validators.required)
     })
+  }
+
+  validadorCorreoEmpresarial(control: FormControl): { [s: string]: boolean } {
+
+    if (control && control.value) {
+      const valor = control.value
+      const partes = valor.split("@")
+
+      if (partes.length != 2) return null
+
+      if (this.listaCorreosGratuitos.indexOf(partes[1]) == -1) return null
+
+      return { correoGratuito: true }
+    }
+
+    return null
   }
 
 
