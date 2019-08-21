f28ddaa5d9edbfefb799c1b3d67622ff15c19965
Eduardo Hoyos <edu.hoyos92@gmail.com>
2019-08-20 21:54:00 -0500

    Rutas (parte 2)

--- Angular/Clase08/appRutas/src/app/listado/listado.component.ts
+++ Angular/Clase08/appRutas/src/app/listado/listado.component.ts
@@ -1,6 +1,6 @@
 import { UsuarioService } from '../usuario.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-listado',
@@ -14,18 +14,38 @@ export class ListadoComponent implements OnInit {
 
   personas = []
 
-  constructor(private activatedRoute: ActivatedRoute, private usuarioService: UsuarioService) { }
+  constructor(private activatedRoute: ActivatedRoute, private usuarioService: UsuarioService, private router: Router) { }
 
   ngOnInit() {
-    this.pag = +this.activatedRoute.snapshot.queryParamMap.get("pag")
-    this.tam = +this.activatedRoute.snapshot.queryParamMap.get("tam")
-    this.ord = this.activatedRoute.snapshot.queryParamMap.get("ord")
+    /*     this.pag = +this.activatedRoute.snapshot.queryParamMap.get("pag")
+        this.tam = +this.activatedRoute.snapshot.queryParamMap.get("tam")
+        this.ord = this.activatedRoute.snapshot.queryParamMap.get("ord") */
 
-    this.listar()
+    this.activatedRoute.queryParamMap.subscribe((data: any) => {
+      /*       this.pag = data.params.pag
+            this.tam = data.params.tam
+            this.ord = data.params.ord */
+
+      const { pag, tam, ord } = data.params
+
+      this.pag = +pag
+      this.tam = +tam
+      this.ord = ord
+
+      this.listar()
+    })
+
+    //this.listar()
   }
 
   listar() {
     this.personas = this.usuarioService.listado(this.pag, this.tam, this.ord)
+  }
+
+  cambiarPagina(pag: number) {
+    /* this.router.navigate(["/usuario", "listado"], { queryParams: { pag, tam: this.tam, ord: this.ord } }) */
+
+    this.router.navigate(["/usuario", "listado"], { queryParams: { pag, rol: "administrador" }, fragment: "admin", queryParamsHandling: "merge" })
   }
 
 }
